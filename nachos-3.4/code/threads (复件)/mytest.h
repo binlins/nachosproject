e59147c1bd9a665a0fdd3a8af731ba22407eeb0e
linbin <981921742@qq.com>
2019-09-26 01:03:52 -0700

    first upload

---
+++ nachos-3.4/code/threads (复件)/mytest.h
@@ -0,0 +1,6 @@
+#include "syscall.h"
+
+#include "copyright.h"
+
+#include "stdio.h"
+void mytest1(){}
---
+++ nachos-3.4/code/threads/mytest.cc
@@ -0,0 +1,14 @@
+#include "syscall.h"
+
+#include "copyright.h"
+
+#include "stdio.h"
+void mytest1() 
+{
+	int which = 1;
+	printf("*** mytest %d times\n", which);
+printf("*** mytest %d times\n", which);
+printf("---------------------------------\n");
+
+
+}
